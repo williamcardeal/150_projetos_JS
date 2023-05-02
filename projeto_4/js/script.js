4da0cb3517db8fc9c4de6fd2c4bb288392c4dff9
williamcardeal <128101285+williamcardeal@users.noreply.github.com>
2023-05-02 16:39:15 -0300

    lista

--- projeto_4/js/script.js
+++ projeto_4/js/script.js
@@ -1,59 +1,58 @@
-function addTask(){
+function addTask() {
+  const taskTile = document.querySelector("#task-title").value;
 
-    const taskTile = document.querySelector("#task-title").value
+  if (taskTile) {
+    //clana template
+    const template = document.querySelector(".template");
 
-    if(taskTile){
-        //clana template
-        const template= document.querySelector(".template")
+    const newTask = template.cloneNode(true);
+    // adicionar titulo
+    newTask.querySelector(".task-title").textContent = taskTile;
+    // remover  as classes
+    newTask.classList.remove("template");
+    newTask.classList.remove("hide");
+    //adicionar tarefa na lista
+    const list = document.querySelector("ul");
 
-        const newTask = template.cloneNode(true)
-        // adicionar titulo
-        newTask.querySelector(".task-title").textContent = taskTile
-          // remover  as classes 
-        newTask.classList.remove("template")
-        newTask.classList.remove("hide")
-          //adicionar tarefa na lista
-        const list =document.querySelector("ul")
+    list.appendChild(newTask);
 
-        list.appendChild(newTask)
+    //adionar o envento remover
+    const removeBtm = newTask
+      .querySelector(".remove-btn")
+      .addEventListener("click", function () {
+        removeTask(this);
+      });
 
-        //adionar o envento remover
-        const removeBtm = newTask.querySelector(".remove-btn").addEventListener("click",function(){
-            removeTask(this)
-        })
+    //adicionar evento de completar tarefa
+    const doneBtn = newTask
+      .querySelector(".done-btn")
+      .addEventListener("click", function () {
+        completeTask(this);
+      });
 
-        //adicionar evento de completar tarefa
-        const doneBtn = newTask.querySelector(".done-btn").addEventListener("click", function(){
-            completeTask(this)
-        })
-
-        //limpar texto
-         document.querySelector("#task-title").value = ""
-    }
-
-} 
+    //limpar texto
+    document.querySelector("#task-title").value = "";
+  }
+}
 
 //fução remover tarefa
-function removeTask(task){
-    task.parentNode.remove(true)
-
+function removeTask(task) {
+  task.parentNode.remove(true);
 }
 
 //função de completar tarefa
-function completeTask(task){
-
-   const taskToComplete = task.parentNode
-//erro
-   taskToComplete.classList.toggle("done")
+function completeTask(task) {
+  const taskToComplete = task.parentNode;
+  //erro
+  taskToComplete.classList.toggle("done");
 }
 
-const addBtn = document.querySelector ("#add-btn")
+const addBtn = document.querySelector("#add-btn");
 
-addBtn.addEventListener("click",function(e){
-    e.preventDefault()
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    console.log("testando")
+  console.log("testando");
 
-    addTask()
-
-})
+  addTask();
+});
